0328fe195a36596d43787750094f7efa7ef7c1f3
Sergey <iseiryu@gmail.com>
2024-07-21 11:48:44 -0400

    refactoring

---
+++ FileInformation/Commands/IRecurseCommand.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using Spectre.Console.Cli;
+
+namespace FileInformation.Cli.Commands;
+internal interface IRecurseCommand {
+    [CommandOption("-r|--recurse")]
+    [DefaultValue(false)]
+    bool RecurseSubdirectories { get; init; }
+
+    [CommandOption("-d|--depth")]
+    [DefaultValue(0)]
+    int Depth { get; init; }
+}
